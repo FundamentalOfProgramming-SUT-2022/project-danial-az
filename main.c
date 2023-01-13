607dbccff37e208dde473636f2a2e6ebd6e742cb
danialazarnia <danialazarnia83@gmail.com>
2023-01-13 13:05:37 +0330

    add creat file.

---
+++ main.c
@@ -0,0 +1,139 @@
+#include<stdio.h>
+#include<string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define root "C:\\Users\\Danial\\Desktop\\root"
+
+
+struct savedirandfile{
+char *saved;
+
+};
+void getcrf(){
+char a[2000];
+int io=1;
+getchar();
+scanf("%c",&a[0]);
+if(a[0]=='"'){
+
+    while(1){
+       scanf("%c",&a[io]);
+        if(a[io]=='"')
+            break;
+        io++;
+    }
+    char op[1000];
+    for(int i=1;i<io;i++)
+        op[i-1]=a[i];
+    creatfile(op);
+}
+else if(a[0]=='r'){
+
+
+    while(1){
+       scanf("%c",&a[io]);
+        if(a[io]=='\n'){
+            break;
+            }
+        io++;
+    }
+char op[1000];
+    for(int i=0;i<io;i++)
+        op[i]=a[i];
+    creatfile(op);
+}
+else
+    printf("wrong address.");
+}
+
+int existfile(const char* name){
+DIR* dir = opendir(name);
+if (dir) {
+        return 1;
+    /* Directory exists. */
+    closedir(dir);
+} else if (ENOENT == errno) {
+    return 0;
+    /* Directory does not exist. */
+} else {
+    return -1;
+
+    /* opendir() failed for some other reason. */
+}
+}
+void creatfile(char *khat){
+char *took;
+char ro[]=root;
+struct savedirandfile a[100];
+int contersaved=0;
+took=strtok(khat,"/");
+while(took!=NULL){
+    a[contersaved].saved=took;
+
+    took=strtok(NULL,"/");
+    contersaved++;
+}
+for(int i=1;i<contersaved-1;i++){
+char o[]="\\";
+strcat(ro,o);
+
+strcat(ro,a[i].saved);
+
+ if(existfile(ro)==0){
+   int c =mkdir(ro);
+    if (!c);
+      //printf("Directory created\n");
+   else {
+      //printf("Unable to create directory\n");
+      exit(1);
+   }
+ }
+    else;
+    //printf("kljsdf");
+}
+char o[]="\\";
+strcat(ro,o);
+strcat(ro,a[contersaved-1].saved);
+
+char po[1500];
+strcpy(po,ro);
+int s=access(ro,F_OK);
+
+if(s==0)
+    printf("this file is exist.");
+else{
+FILE *ptr;
+ptr=fopen(po,"w");
+if(ptr == NULL)
+   {
+      printf("Error!");
+      exit(1);
+   }
+   printf("your file created");
+fclose(ptr);
+}
+
+
+}
+
+
+
+int main(){
+char vorodi[1500];
+scanf("%s",vorodi);
+
+
+if(strcmp(vorodi,"crf")==0){
+char yoi[10];
+scanf("%s",yoi);
+
+if(strcmp(yoi,"--file")==0)
+    getcrf();
+}
+return 0;
+
+}
